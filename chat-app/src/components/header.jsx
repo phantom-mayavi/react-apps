fa56f5162cec349487cd1b28ea06c7c20d48cf1e
phantom-mayavi <phantom.mayavi@gmail.com>
2021-09-01 20:25:02 -0400

    header section

--- chat-app/src/components/header.jsx
+++ chat-app/src/components/header.jsx
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
+import chatapplogo from '../images/chatapp.PNG';
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBCollapse
+  } from 'mdb-react-ui-kit';
 
 class Header extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>Header !!!</h1> );
+    state = {};
+
+    render() {
+        return (
+            <div>
+                <MDBNavbar expand='lg' light bgColor='light'>
+                    <MDBContainer fluid>
+                        <MDBNavbarBrand href='#' margin-right="-14rem">
+                            <img src={chatapplogo} alt="ChatAppLogo" width="50%" height="50%"/>
+                        </MDBNavbarBrand>
+                        <MDBCollapse navbar show={true}>
+                            <MDBNavbarNav>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink active aria-current='page' href='#'>
+                                        Home
+                                    </MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Team</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <span>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </span>
+
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Docs</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Contact</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Login</MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <button type="button" class="btn btn-primary">Call Us</button>
+                                </MDBNavbarItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBContainer>
+                </MDBNavbar>
+            </div>
+        );
     }
 }
  
